15341d98e4d3e684b44b0945620b23c22f9108ea
AMiller90 <A_Miller729@yahoo.com>
2016-11-04 22:22:58 -0500

    First Commit
    Added player, bullet, player and bullet movement, set up game scene and
    have mouse rotation for shooting

---
+++ ComplexGame/Assets/Scripts/Bullet.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour
+{
+    private float speed;
+    private Vector3 direction;
+
+    public float Speed
+    {
+        get { return speed; }
+        set { speed = value; }
+    }
+
+    public Vector3 Direction
+    {
+        get { return direction; }
+        set { direction = value; }
+    }
+
+	// Update is called once per frame
+	void Update ()
+	{
+	    transform.position += direction * (Time.deltaTime *speed);
+        BulletLifeTime();
+	}
+
+    private void BulletLifeTime()
+    {
+        if (transform.position.x <= -18.5 || transform.position.x >= 18.5)
+            Destroy(gameObject);
+        if (transform.position.y <= 1.5f || transform.position.y >= 27.5f)
+            Destroy(gameObject);
+    }
+}
---
+++ ComplexGame/Assets/Scripts/Player.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bullet;
+    [SerializeField]
+    private Transform muzzleTransform;
+
+    private float speed = 5;
+
+	// Use this for initialization
+	void Start ()
+	{
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        CheckBounds();
+    
+        Move();
+
+	    Shoot();
+	}
+
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.W))
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.S))
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.D))
+            transform.position += Vector3.right * speed * Time.deltaTime;
+    }
+
+    private void Shoot()
+    {
+        Vector3 displacement = (Input.mousePosition - transform.position);
+        displacement.Normalize();
+
+        Debug.DrawLine(muzzleTransform.position, Input.mousePosition);
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            GameObject gbullet = Instantiate(bullet, muzzleTransform.position, muzzleTransform.rotation) as GameObject;
+            gbullet.GetComponent<Bullet>().Speed = 10;
+            gbullet.GetComponent<Bullet>().Direction = displacement;
+        }
+       
+    }
+
+    private void CheckBounds()
+    {
+        if (transform.position.x <= -18.5)
+            transform.position = new Vector3(-18.5f,transform.position.y,transform.position.z);
+        if (transform.position.x >= 18.5)
+            transform.position = new Vector3(18.5f, transform.position.y, transform.position.z);
+        if (transform.position.y <= 1.5f)
+            transform.position = new Vector3(transform.position.x, 1.5f, transform.position.z);
+        if (transform.position.y >= 28.0f)
+            transform.position = new Vector3(transform.position.x, 28.0f, transform.position.z);
+    }
+}
