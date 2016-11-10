4cf0fbc7acb6601577ede62eb5f89d964679bc04
AMiller90 <A_Miller729@yahoo.com>
2016-11-09 20:50:49 -0600

    Added Player sprite and animator
    Player now has a sprite and animations

--- ComplexGame/Assets/Scripts/Player.cs
+++ ComplexGame/Assets/Scripts/Player.cs
@@ -4,10 +4,21 @@ using UnityEditor;
 
 public class Player : MonoBehaviour
 {
+    private bool facingRight;
+
     [SerializeField]
     private GameObject bullet;
+    [SerializeField]
+    private Transform armTransform;
+    [SerializeField]
+    private Transform bodyTransform;
+    [SerializeField]
+    private Transform firePointTransform;
 
-    private Transform muzzleTransform;
+    [SerializeField]
+    private Animator anim;
+
+    private int jumpHash = Animator.StringToHash("Jump");
 
     private bool canJump;
 
@@ -26,13 +37,24 @@ public class Player : MonoBehaviour
 	void Start ()
 	{
 	    health = 100;
-	    speed = 5;
+	    speed = 0;
 
 	    if (playerBody == null)
 	        playerBody = GetComponent<Rigidbody>();
 
-        if (muzzleTransform == null)
-            muzzleTransform = GetComponentsInChildren<Transform>()[1];
+        if (armTransform == null)
+            armTransform = GetComponentsInChildren<Transform>()[2];
+
+        if(bodyTransform == null)
+            bodyTransform = GetComponentsInChildren<Transform>()[1];
+
+        if (firePointTransform == null)
+            firePointTransform = GetComponentsInChildren<Transform>()[4];
+
+	    if (anim == null)
+	    {
+	        anim = GetComponent<Animator>();
+	    }
     }
 	
 	// Update is called once per frame
@@ -45,13 +67,33 @@ public class Player : MonoBehaviour
 	}
 
     private void Move()
-    {//Move to the left
+    {
+        anim.SetFloat("Speed", 0);
+
+        if (Input.GetKeyDown(KeyCode.A) && !facingRight)
+        {
+            Flip();
+        }
+
+        if (Input.GetKeyDown(KeyCode.D) && facingRight)
+        {
+            Flip();
+        }
+            
+
+        //Move to the left
         if (Input.GetKey(KeyCode.A))
-            transform.position += Vector3.left * speed * Time.deltaTime;
+        {
+            anim.SetFloat("Speed", 5);
+            transform.position += Vector3.left * 5 * Time.deltaTime;
+        }
+            
         //Move to the right
         if (Input.GetKey(KeyCode.D))
-            transform.position += Vector3.right * speed * Time.deltaTime;
-
+        {
+            anim.SetFloat("Speed", 5);
+            transform.position += Vector3.right * 5 * Time.deltaTime;
+        }
     }
 
     private void Shoot()
@@ -62,7 +104,7 @@ public class Player : MonoBehaviour
         mousePosition.z = Camera.main.transform.position.z * -1;
 
         //Get the displacement between mouse an muzzle position
-        Vector3 displacement = Camera.main.ScreenToWorldPoint(mousePosition) - muzzleTransform.position;
+        Vector3 displacement = Camera.main.ScreenToWorldPoint(mousePosition) - armTransform.position;
         //Get the direction
         Vector3 bulletdirection = displacement.normalized;
 
@@ -70,14 +112,14 @@ public class Player : MonoBehaviour
         float rotZ = Mathf.Atan2(bulletdirection.x, bulletdirection.y)*Mathf.Rad2Deg;
 
         //Rotate the z axis as the mouse moves
-        transform.rotation = Quaternion.Euler(0f, 0f, -rotZ);
+        armTransform.rotation = Quaternion.Euler(0f, 0f, -rotZ + 90);
 
-        Debug.DrawLine(muzzleTransform.position, Camera.main.ScreenToWorldPoint(mousePosition));
+        Debug.DrawLine(armTransform.position, Camera.main.ScreenToWorldPoint(mousePosition));
 
         //If left click
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {//Instantiate a bullet
-            GameObject gbullet = Instantiate(bullet, muzzleTransform.position, muzzleTransform.rotation) as GameObject;
+            GameObject gbullet = Instantiate(bullet, firePointTransform.position, firePointTransform.rotation) as GameObject;
 
             //Add the bullet component if it doesnt have one
             if (!gbullet.GetComponent<Bullet>())
@@ -94,11 +136,20 @@ public class Player : MonoBehaviour
 
     private void Jump()
     {
+        anim.SetBool("IsGrounded", canJump);
         if (Input.GetKeyDown(KeyCode.Space) && canJump)
         {
            canJump = false;
            playerBody.velocity = new Vector3(playerBody.velocity.x, 10, 0);
         }
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = bodyTransform.localScale;
+        theScale.x *= -1;
+        bodyTransform.localScale = theScale;
     }
 
     void OnTriggerEnter(Collider other)
