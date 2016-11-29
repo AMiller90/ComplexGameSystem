67a9ef5727b25cbd7fb713cc51abbc6b67ab64fd
Andrew Miller <A_Miller729@yahoo.com>
2016-11-29 17:16:03 -0600

    Practicing with lobby
    Working on making own lobby

--- ComplexGame/Assets/Scripts/HostGame.cs
+++ ComplexGame/Assets/Scripts/HostGame.cs
@@ -1,15 +1,35 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 using System.Collections;
 
-public class HostGame : MonoBehaviour {
+public class HostGame : MonoBehaviour{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private uint roomSize = 4;
+
+    private string roomName = "";
+
+    private NetworkManager networkManager;
+
+    void Start()
+    {
+        networkManager = NetworkManager.singleton;
+
+        if (networkManager.matchMaker == null)
+            networkManager.StartMatchMaker();
+    }
+
+    public void SetRoomName(string name)
+    {
+        roomName = name;
+    } 
+
+    public void CreateRoom()
+    {
+        if (roomName != "" && roomName != null)
+        {
+            Debug.Log("Creating a room: " + roomName + " with room for " + roomSize + " players.");
+            networkManager.matchMaker.CreateMatch(roomName, roomSize, true, "", "","", 0, 1, networkManager.OnMatchCreate);
+        }
+            
+    }
 }
---
+++ ComplexGame/Assets/Scripts/JoinGame.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+
+public class JoinGame : MonoBehaviour {
+
+    List<GameObject> roomList = new List<GameObject>();
+
+    [SerializeField]
+    private Text status;
+
+    [SerializeField]
+    private GameObject roomListItemPrefab;
+
+    [SerializeField]
+    private Transform roomListParent;
+
+    private NetworkManager networkManager;
+
+    void Start()
+    {
+        networkManager = NetworkManager.singleton;
+
+        if(networkManager.matchMaker == null)
+        {
+            networkManager.StartMatchMaker();
+        }
+
+        RefreshRoomList();
+    }
+
+    public void RefreshRoomList()
+    {
+        networkManager.matchMaker.ListMatches(0, 20, "", false, 0, 1, OnMatchList);
+        status.text = "Loading...";
+    }
+
+    public void OnMatchList(bool success, string extendedInfo, List<MatchInfoSnapshot> matches)
+    {
+        status.text = "";
+
+        if(matches == null)
+        {
+            status.text = "Couldn't get room list.";
+            return;
+        }
+
+        ClearRoomList();
+
+        foreach (MatchInfoSnapshot match in matches)
+        {
+            GameObject _roomListItemGO = Instantiate(roomListItemPrefab);
+            _roomListItemGO.transform.SetParent(roomListParent);
+        }
+    }
+
+    private void ClearRoomList()
+    {
+        for (int i = 0; i < roomList.Count; i++)
+        {
+            Destroy(roomList[i]);
+        }
+
+        roomList.Clear();
+    }
+}
